9b4e5c55095198263691bdbfb1dc0690c25d80e3
rhoouma <71633763+rhoumaesen@users.noreply.github.com>
2022-05-20 16:11:42 +0000

    changed main to be daytime not dict

--- Main.java
+++ Main.java
@@ -3,32 +3,24 @@ import java.io.*;
 
 public class Main {
 
-  public static final String SERVER = "all.dict.org";
-  public static final int PORT = 2628;
-  public static final int TIMEOUT = 20000;
+  
   
   public static void main(String[] args) {
-
+String hostname = args.length > 0 ? args[0] : "time.nist.gov";
     Socket socket = null;
     try {
-      socket = new Socket(SERVER, PORT);
-      socket.setSoTimeout(TIMEOUT);
-      OutputStream out = socket.getOutputStream();
-      Writer writer = new OutputStreamWriter(out, "UTF-8");
-      writer = new BufferedWriter(writer);
+      socket = new Socket(hostname, 13);
+      socket.setSoTimeout(15000);
       InputStream in = socket.getInputStream();
-      BufferedReader reader = new BufferedReader(
-          new InputStreamReader(in, "UTF-8"));
-      
-      for (String word : args) {
-        define(word, writer, reader);
+      StringBuilder time = new StringBuilder();
+      InputStreamReader reader = new InputStreamReader(in, "ASCII");
+      for (int c = reader.read(); c != -1; c = reader.read()) {
+        time.append((char) c);
       }
-      
-      writer.write("quit\r\n");
-      writer.flush();
+      System.out.println(time);
     } catch (IOException ex) {
       System.err.println(ex);
-    } finally { // dispose
+    } finally {
       if (socket != null) {
         try {
           socket.close();
@@ -37,23 +29,6 @@ public class Main {
         }
       }
     }
-  }
-
-  static void define(String word, Writer writer, BufferedReader reader)
-      throws IOException, UnsupportedEncodingException {
-    writer.write("DEFINE eng-lat " + word + "\r\n");
-    writer.flush();
-
-    for (String line = reader.readLine(); line != null; line = reader.readLine()) {
-      if (line.startsWith("250 ")) { // OK
-        return;
-      } else if (line.startsWith("552 ")) { // no match
-        System.out.println("No definition found for " + word);
-        return;
-      }
-      else if (line.matches("\\d\\d\\d .*")) continue;
-      else if (line.trim().equals(".")) continue;
-      else System.out.println(line);
-    }
+    
   } 
 }
